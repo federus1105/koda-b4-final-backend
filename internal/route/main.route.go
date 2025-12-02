dfbcdb9d9fd729f606464c41beefe99c86640ff7
federus1105 <federusrudi@gmail.com>
2025-12-02 17:47:59 +0700

    Feat ( routes ) : feature init routes profile

--- internal/route/main.route.go
+++ internal/route/main.route.go
@@ -14,6 +14,7 @@ func InitRouter(app *gin.Engine, db *pgxpool.Pool, rd *redis.Client) {
 	// --- ROUTE ---
 	InitAuthRouter(app, db)
 	InitShortLinkRouter(app, db, rd)
+	InitProfileRouter(app, db, rd)
 
 	app.NoRoute(func(ctx *gin.Context) {
 		ctx.JSON(404, models.ResponseFailed{
