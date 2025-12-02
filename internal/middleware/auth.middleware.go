2f94a5ccf99544dce1d10ff6ded4a61320ac80e5
federus1105 <federusrudi@gmail.com>
2025-12-02 16:20:51 +0700

    Fix : move optional middleware functions to their own files

--- internal/middleware/auth.middleware.go
+++ internal/middleware/auth.middleware.go
@@ -51,30 +51,3 @@ func AuthMiddleware() gin.HandlerFunc {
 		c.Next()
 	}
 }
-
-func OptionalAuth() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		authHeader := c.GetHeader("Authorization")
-
-		// --- IF NULL -> ANONYMOUS ---
-		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
-			c.Next()
-			return
-		}
-
-		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-		claims := &libs.Claims{}
-		if err := claims.VerifyToken(tokenString); err != nil {
-			c.Next()
-			return
-		}
-
-		c.Set(UserIDKey, claims.ID)
-		c.Set("role", claims.Role)
-
-		ctx := context.WithValue(c.Request.Context(), UserIDKey, claims.ID)
-		c.Request = c.Request.WithContext(ctx)
-
-		c.Next()
-	}
-}
